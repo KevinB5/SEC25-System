f551bab6f2f4cb5cc60181ac245deceedd7e5fa6
cintixx <cintia.almeida@tecnico.ulisboa.pt>
2019-03-19 21:28:54 +0000

    erros nas POMs corrigido, ambos os projetos a compilar, criado um projeto source

--- proj-Server/src/main/java/pt/tecnico/sec/App.java
+++ proj-Server/src/main/java/pt/tecnico/sec/App.java
@@ -1,5 +1,8 @@
 package pt.tecnico.sec;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Aplicacao principal, lanca o notario e o Public Key Infraestructure
  *
@@ -18,7 +21,24 @@ public class App
     	//pki gera as chaves
     	//pki atribui a chave ao user que ligou
     	//notario recebe pedidos do cliente
+    	System.out.println("Fg mano");
+        if (args.length < 1) {
+            System.err.println("Argument(s) missing!");
+            System.err.printf("Usage: java %s port%n", ServerSocket.class.getName());
+            return;
+        }
+
+        // Convert port from String to int
+        int port = Integer.parseInt(args[0]);
+
+        // Create server socket
+        try {
+			ServerSocket serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.printf("Server accepting connections on port %d %n", port);
     }
-    
-    
+      
 }
