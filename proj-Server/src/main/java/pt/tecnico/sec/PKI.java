c41b6d376628c4bb77838306cf3cd587727ed38f
Kevin <kevin-batistac@hotmail.com>
2019-03-20 15:44:46 +0000

    RSA

--- proj-Server/src/main/java/pt/tecnico/sec/PKI.java
+++ proj-Server/src/main/java/pt/tecnico/sec/PKI.java
@@ -8,73 +8,35 @@ import java.util.HashMap;
 
 import static javax.xml.bind.DatatypeConverter.printHexBinary;
 
+import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 public class PKI {
 
+	private final int keySize = 1024;
 	private HashMap <String,PublicKey> keys = new HashMap <String,PublicKey>();
 	
-/*public static void main(String[] args) throws Exception {
-	
-    // check args
-    if (args.length != 3) {
-        System.err.println("Usage: RSAKeyGenerator [r|w] <priv-key-file> <pub-key-file>");
-        return;
-    }
-
-    final String mode = args[0];
-    final String privkeyPath = args[1];
-    final String pubkeyPath = args[2];
-
-    if (mode.toLowerCase().startsWith("w")) {
-        System.out.println("Generate and save keys");
-        write(privkeyPath);
-        write(pubkeyPath);
-    } else {
-        System.out.println("Load keys");
-        read(privkeyPath);
-        read(pubkeyPath);            
-    }
-
-    System.out.println("Done.");
-}
-*/
-
-public static void write(String keyPath) throws GeneralSecurityException, IOException {
-    // get an AES private key
-    System.out.println("Generating RSA key ..." );
+public KeyPair createKeys(String keyPath) throws GeneralSecurityException, IOException {
     KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-    keyGen.initialize(1024);
+    keyGen.initialize(this.keySize);
     KeyPair keys = keyGen.generateKeyPair();
-    System.out.println("Finish generating RSA keys");
     
-    System.out.println("Private Key:");
-    PrivateKey privKey = keys.getPrivate();
-    byte[] privKeyEncoded = privKey.getEncoded();
-    System.out.println(printHexBinary(privKeyEncoded));
-    System.out.println("Public Key:");
-    PublicKey pubKey = keys.getPublic();
-    byte[] pubKeyEncoded = pubKey.getEncoded();
-    System.out.println(printHexBinary(pubKeyEncoded));       
-
-    System.out.println("Writing Private key to '" + keyPath + "' ..." );
-    FileOutputStream privFos = new FileOutputStream(keyPath);
-    privFos.write(privKeyEncoded);
-    privFos.close();
-    System.out.println("Writing Pubic key to '" + keyPath + "' ..." );
-    FileOutputStream pubFos = new FileOutputStream(keyPath);
-    pubFos.write(pubKeyEncoded);
-    pubFos.close();        
+    return keys;
 }
 
-public static Key read(String keyPath) throws GeneralSecurityException, IOException {
-    System.out.println("Reading key from file " + keyPath + " ...");
-    FileInputStream fis = new FileInputStream(keyPath);
-    byte[] encoded = new byte[fis.available()];
-    fis.read(encoded);
-    fis.close();
+public byte[] encrypt(PrivateKey privateKey, String message) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA");  
+    cipher.init(Cipher.ENCRYPT_MODE, privateKey);  
 
-    return new SecretKeySpec(encoded, "RSA");
+    return cipher.doFinal(message.getBytes());  
 }
+
+public byte[] decrypt(PublicKey publicKey, byte [] encrypted) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA");  
+    cipher.init(Cipher.DECRYPT_MODE, publicKey);
+    
+    return cipher.doFinal(encrypted);
+}
+    
 
 }
