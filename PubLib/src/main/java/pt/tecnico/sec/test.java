5cefd068b7c8f0586af47643f5bdd87ecd742e64
cintixx <cintia.almeida@tecnico.ulisboa.pt>
2019-04-11 19:33:18 +0100

    teste

--- PubLib/src/main/java/pt/tecnico/sec/test.java
+++ PubLib/src/main/java/pt/tecnico/sec/test.java
@@ -1,31 +1,34 @@
 package pt.tecnico.sec;
 
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import pt.tecnico.sec.eIDLib;
 
 public class test {
+	private static eIDLib eid;
 
 	public static void main(String[] args) {
-		eIDLib eid = new eIDLib();
-		//eid.start();
-		X509Certificate cert = null;
-		/*
-		try {
-			//cert = PKI.generateCertificate("user1" , 7, "SHA1withRSA");
-		} catch (GeneralSecurityException e) {
-			// TODO Auto-generated catch block
-			
-			e.printStackTrace();
-		} catch (IOException e) {
+		start();
+	}
+	
+	private static void start() {
+		eid = new eIDLib();
+		X509Certificate cert = null; 
+         try {
+			cert=eIDLib.getCertFromByteArray(eIDLib.getCertificateInBytes(0));
+		} catch (CertificateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 		
 		if(eid.verifySignature(eid.sign(cert,"teste"),"teste"))
 			System.out.println("VALIDOU");
 		else
 			System.out.println("MACACO");
+
 	}
 
 }
