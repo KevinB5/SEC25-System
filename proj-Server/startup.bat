9f8d7e9f7e042dc0a818585cf8ab51cb6c8dec05
KevinB5 <kbcsa1@iscte-iul.pt>
2019-05-12 21:34:42 +0100

    Update startup.bat

--- proj-Server/startup.bat
+++ proj-Server/startup.bat
@@ -3,7 +3,7 @@ SET /A i = 1
 :loop
 
 IF %i%==5 GOTO END 
-	start cmd.exe /k "mvn clean compile exec:java -Dexec.args=%i%"
+	start cmd.exe /k "mvn clean compile exec:java -Dexec.args=8000 %i%"
 	SET /a i=%i%+1
 GOTO :LOOP
 :END
