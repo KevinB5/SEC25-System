91bf3d4ca9fd2cd92c72ac952ca64ab5961d760a
ist194131 <ist194131@lab8p6>
2019-05-13 16:08:17 +0100

    CERTIFICADO FALSO FUNCIONA

--- PubLib/src/main/java/pt/tecnico/sec/eIDLib.java
+++ PubLib/src/main/java/pt/tecnico/sec/eIDLib.java
@@ -54,6 +54,7 @@ public class eIDLib{
 	private static Signature sig;
 	private static X509Certificate cert;
 	private static String line = System.getProperty("file.separator");
+	private static boolean cardExist = true;
 	
  
 	public static X509Certificate getCert() {
@@ -67,7 +68,7 @@ public class eIDLib{
 			
 			String path = System.getProperty("user.dir")+line+"Temp"+line;
 			
-//			path = path.replaceAll("\\\\","/");
+			path = path.replaceAll("\\\\","/");
 			System.out.println(path);
 	        System.setProperty("java.library.path", path);
 	        Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
@@ -85,252 +86,125 @@ public class eIDLib{
 				| IllegalAccessException | SecurityException | IllegalArgumentException | UnsatisfiedLinkError e ) {
 			CertificateFactory cf;
 			try {
-				System.out.println("Fake Certificate");
+				cardExist = false;
 				cf = CertificateFactory.getInstance("X.509");
 				cert = (X509Certificate)cf.generateCertificate(new FileInputStream(
 						System.getProperty("user.dir")+line+"lib"+line+"certificate.crt"));
+				System.out.println("PRINT: " +cert.getSigAlgName());
 				sig = Signature.getInstance(cert.getSigAlgName());
-				System.out.println("2 "+cert);
 			} catch (CertificateException | FileNotFoundException | NoSuchAlgorithmException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-    		System.out.println(cert);
 //			e.printStackTrace();
 		}
 		
 	}
-//	public void start() {
-//		 try
-//	        {
-//	            
-////	        System.out.println("            //Load the PTEidlibj");
-////	        System.setProperty("java.library.path", "/usr/local/lib/");
-////	        Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
-////	        fieldSysPath.setAccessible(true);
-////	        fieldSysPath.set(null, null);
-////	        System.loadLibrary("pteidlibj");
-////	        pteid.Init(""); // Initializes the eID Lib
-////	        pteid.SetSODChecking(false); // Don't check the integrity of the ID, address and photo (!)
-//
-//	        PKCS11 pkcs11;
-//	        String osName = System.getProperty("os.name");
-//	        String javaVersion = System.getProperty("java.version");
-//	    
-//	        java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
-//	     
-//	        String libName = "libpteidpkcs11.so";
-//	       
-//	        
-//	        X509Certificate cert=getCertFromByteArray(getCertificateInBytes(0));
-//	        
-//	        //Código de assinar
-////	        Signature sig = Signature.getInstance(getCertFromByteArray(getCertificateInBytes(0)).getSigAlgName());
-//	        sig.initVerify(cert);
-//	        
-//	        if (-1 != osName.indexOf("Windows"))
-//                libName = "pteidpkcs11.dll";
-//            else if (-1 != osName.indexOf("Mac"))
-//                libName = "pteidpkcs11.dylib";
-//            Class pkcs11Class = Class.forName("sun.security.pkcs11.wrapper.PKCS11");
-//            if (javaVersion.startsWith("1.5."))
-//            {
-//                Method getInstanceMethode = pkcs11Class.getDeclaredMethod("getInstance", new Class[] { String.class, CK_C_INITIALIZE_ARGS.class, boolean.class });
-//                pkcs11 = (PKCS11)getInstanceMethode.invoke(null, new Object[] { libName, null, false });
-//            }
-//            else
-//            {
-//                Method getInstanceMethode = pkcs11Class.getDeclaredMethod("getInstance", new Class[] { String.class, String.class, CK_C_INITIALIZE_ARGS.class, boolean.class });
-//                pkcs11 = (PKCS11)getInstanceMethode.invoke(null, new Object[] { libName, "C_GetFunctionList", null, false });
-//            }
-//	        
-//            //Open the PKCS11 session
-//            System.out.println("            //Open the PKCS11 session");
-//            long p11_session = pkcs11.C_OpenSession(0, PKCS11Constants.CKF_SERIAL_SESSION, null, null);
-//            
-//            // Token login 
-//            System.out.println("            //Token login");
-//            pkcs11.C_Login(p11_session, 1, null);
-//            CK_SESSION_INFO info = pkcs11.C_GetSessionInfo(p11_session);
-//            
-//            // Get available keys
-//            System.out.println("            //Get available keys");
-//            CK_ATTRIBUTE[] attributes = new CK_ATTRIBUTE[1];
-//            attributes[0] = new CK_ATTRIBUTE();
-//            attributes[0].type = PKCS11Constants.CKA_CLASS;
-//            attributes[0].pValue = new Long(PKCS11Constants.CKO_PRIVATE_KEY);
-//            
-//            pkcs11.C_FindObjectsInit(p11_session, attributes);
-//            long[] keyHandles = pkcs11.C_FindObjects(p11_session, 5);
-//            
-//            // points to auth_key
-//            System.out.println("            //points to auth_key. No. of keys:"+keyHandles.length);
-//            
-//            long signatureKey = keyHandles[0];		//test with other keys to see what you get
-//            pkcs11.C_FindObjectsFinal(p11_session);
-//            
-//            
-//            // initialize the signature method
-//            System.out.println("            //initialize the signature method");
-//            CK_MECHANISM mechanism = new CK_MECHANISM();
-//            mechanism.mechanism = PKCS11Constants.CKM_SHA1_RSA_PKCS;
-//            mechanism.pParameter = null;
-//            pkcs11.C_SignInit(p11_session, mechanism, signatureKey);
-//	        
-//            
-//            //Assinatura para usar 
-//            //Lab 2 editar
-//            
-//            byte[] signature = pkcs11.C_Sign(p11_session, "data".getBytes(Charset.forName("UTF-8")));
-//
-//            
-//            //Verificar assinature
-//            sig.update("data".getBytes(Charset.forName("UTF-8")));
-//           
-//            
-//            if(sig.verify(signature)) {
-//            	System.out.println("            //Valid");
-//            }else {
-//            	System.out.println("            //Not valid");
-//            }
-//            
-//            pteid.Exit(pteid.PTEID_EXIT_LEAVE_CARD); //OBRIGATORIO Termina a eID Lib
-//	        }  catch (Throwable e)
-//	        {
-//	            System.out.println("[Catch] Exception: " + e.getMessage());
-//	            e.printStackTrace();
-//		    }
-//	}
 	
 	public byte[] sign(X509Certificate cert,String data) {
-		PKCS11 pkcs11 = null;
-        String osName = System.getProperty("os.name");
-        String javaVersion = System.getProperty("java.version");
-        long p11_session = 0;
-        byte[] signature = null;
-        
-        String path = System.getProperty("user.dir")+line+"Temp"+line;
-		
-//		path = path.replaceAll("\\\\","/");
-		System.out.println(path);
-        System.setProperty("java.library.path", path);
-        Field fieldSysPath;
-		try {
-			
-			fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
-        fieldSysPath.setAccessible(true);
-        System.out.println(System.getProperty("java.library.path"));
-        System.loadLibrary("pteidlibj");
-        pteid.Init("");
-        pteid.SetSODChecking(false);
-        fieldSysPath.set(null, null);
-		} catch (PteidException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		 // Initializes the eID Lib
-        
-			} catch (NoSuchFieldException e2) {
+		byte[] signature = null;
+		if(cardExist) {
+			PKCS11 pkcs11 = null;
+	        String osName = System.getProperty("os.name");
+	        String javaVersion = System.getProperty("java.version");
+	        long p11_session = 0;
+	        
+	        java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
+	     
+	        String libName = "libpteidpkcs11.so";
+			 try {
+				sig.initVerify(cert);
+			} catch (InvalidKeyException e) {
 				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			} catch (SecurityException e2) {
+				e.printStackTrace();
+			}
+		        
+		        if (-1 != osName.indexOf("Windows"))
+	             libName = "pteidpkcs11.dll";
+	         else if (-1 != osName.indexOf("Mac"))
+	             libName = "pteidpkcs11.dylib";
+	         Class pkcs11Class;
+			try {
+				pkcs11Class = Class.forName("sun.security.pkcs11.wrapper.PKCS11");
+	         if (javaVersion.startsWith("1.5."))
+	         {
+	             Method getInstanceMethode = pkcs11Class.getDeclaredMethod("getInstance", new Class[] { String.class, CK_C_INITIALIZE_ARGS.class, boolean.class });
+	             pkcs11 = (PKCS11)getInstanceMethode.invoke(null, new Object[] { libName, null, false });
+	         }
+	         else
+	         {
+	             Method getInstanceMethode = pkcs11Class.getDeclaredMethod("getInstance", new Class[] { String.class, String.class, CK_C_INITIALIZE_ARGS.class, boolean.class });
+	             pkcs11 = (PKCS11)getInstanceMethode.invoke(null, new Object[] { libName, "C_GetFunctionList", null, false });
+	         }
+		        
+	         //Open the PKCS11 session
+	         System.out.println("            //Open the PKCS11 session");
+	         p11_session = pkcs11.C_OpenSession(0, PKCS11Constants.CKF_SERIAL_SESSION, null, null);
+	         
+	         // Token login 
+	         System.out.println("            //Token login");
+	         pkcs11.C_Login(p11_session, 1, null);
+	         CK_SESSION_INFO info = pkcs11.C_GetSessionInfo(p11_session);
+	         
+	         // Get available keys
+	         System.out.println("            //Get available keys");
+	         CK_ATTRIBUTE[] attributes = new CK_ATTRIBUTE[1];
+	         attributes[0] = new CK_ATTRIBUTE();
+	         attributes[0].type = PKCS11Constants.CKA_CLASS;
+	         attributes[0].pValue = new Long(PKCS11Constants.CKO_PRIVATE_KEY);
+	         
+	         pkcs11.C_FindObjectsInit(p11_session, attributes);
+	         long[] keyHandles = pkcs11.C_FindObjects(p11_session, 5);
+	         
+	         // points to auth_key
+	         System.out.println("            //points to auth_key. No. of keys:"+keyHandles.length);
+	         
+	         long signatureKey = keyHandles[0];		//test with other keys to see what you get
+	         pkcs11.C_FindObjectsFinal(p11_session);
+	         
+	         
+	         // initialize the signature method
+	         System.out.println("            //initialize the signature method");
+	         CK_MECHANISM mechanism = new CK_MECHANISM();
+	         mechanism.mechanism = PKCS11Constants.CKM_SHA256_RSA_PKCS;
+	         mechanism.pParameter = null;
+	         pkcs11.C_SignInit(p11_session, mechanism, signatureKey);
+		        
+	         System.out.println("ASSINANDO: "+ data.getBytes(Charset.forName("UTF-8")));
+	         signature = pkcs11.C_Sign(p11_session, data.getBytes(Charset.forName("UTF-8")));
+	         
+	         //Assinatura para usar 
+	         //Lab 2 editar
+			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			
-		} catch (IllegalArgumentException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IllegalAccessException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-        java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
-     
-        String libName = "libpteidpkcs11.so";
-		 try {
-			sig.initVerify(cert);
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	        
-	        if (-1 != osName.indexOf("Windows"))
-             libName = "pteidpkcs11.dll";
-         else if (-1 != osName.indexOf("Mac"))
-             libName = "pteidpkcs11.dylib";
-         Class pkcs11Class;
-		try {
-			pkcs11Class = Class.forName("sun.security.pkcs11.wrapper.PKCS11");
-         if (javaVersion.startsWith("1.5."))
-         {
-             Method getInstanceMethode = pkcs11Class.getDeclaredMethod("getInstance", new Class[] { String.class, CK_C_INITIALIZE_ARGS.class, boolean.class });
-             pkcs11 = (PKCS11)getInstanceMethode.invoke(null, new Object[] { libName, null, false });
-         }
-         else
-         {
-             Method getInstanceMethode = pkcs11Class.getDeclaredMethod("getInstance", new Class[] { String.class, String.class, CK_C_INITIALIZE_ARGS.class, boolean.class });
-             pkcs11 = (PKCS11)getInstanceMethode.invoke(null, new Object[] { libName, "C_GetFunctionList", null, false });
-         }
-	        
-         //Open the PKCS11 session
-         System.out.println("            //Open the PKCS11 session");
-         p11_session = pkcs11.C_OpenSession(0, PKCS11Constants.CKF_SERIAL_SESSION, null, null);
-         
-         // Token login 
-         System.out.println("            //Token login");
-         pkcs11.C_Login(p11_session, 1, null);
-         CK_SESSION_INFO info = pkcs11.C_GetSessionInfo(p11_session);
-         
-         // Get available keys
-         System.out.println("            //Get available keys");
-         CK_ATTRIBUTE[] attributes = new CK_ATTRIBUTE[1];
-         attributes[0] = new CK_ATTRIBUTE();
-         attributes[0].type = PKCS11Constants.CKA_CLASS;
-         attributes[0].pValue = new Long(PKCS11Constants.CKO_PRIVATE_KEY);
-         
-         pkcs11.C_FindObjectsInit(p11_session, attributes);
-         long[] keyHandles = pkcs11.C_FindObjects(p11_session, 5);
-         
-         // points to auth_key
-         System.out.println("            //points to auth_key. No. of keys:"+keyHandles.length);
-         
-         long signatureKey = keyHandles[0];		//test with other keys to see what you get
-         pkcs11.C_FindObjectsFinal(p11_session);
-         
-         
-         // initialize the signature method
-         System.out.println("            //initialize the signature method");
-         CK_MECHANISM mechanism = new CK_MECHANISM();
-         mechanism.mechanism = PKCS11Constants.CKM_SHA256_RSA_PKCS;
-         mechanism.pParameter = null;
-         pkcs11.C_SignInit(p11_session, mechanism, signatureKey);
-	        
-         System.out.println("ASSINANDO: "+ data.getBytes(Charset.forName("UTF-8")));
-         signature = pkcs11.C_Sign(p11_session, data.getBytes(Charset.forName("UTF-8")));
-         
-         //Assinatura para usar 
-         //Lab 2 editar
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (PKCS11Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (PKCS11Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				sig.initVerify(cert);
+			} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			signature = cert.getSignature();
 		}
 		return signature;
          
@@ -353,8 +227,10 @@ public class eIDLib{
         } catch (PteidException e) {
             //e.printStackTrace();
         	try {
+        		cardExist = false;
         		CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        		cert = (X509Certificate)cf.generateCertificate(new FileInputStream(System.getProperty("user.dir")+"/test.cert"));
+        		cert = (X509Certificate)cf.generateCertificate(new FileInputStream(
+						System.getProperty("user.dir")+line+"lib"+line+"certificate.crt"));
         		sig = Signature.getInstance(cert.getSigAlgName());
         		System.out.println(cert.getSigAlgName());
         		System.out.println(cert);
