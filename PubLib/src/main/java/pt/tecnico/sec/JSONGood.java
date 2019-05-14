cbc3ef5a60deeca1e6b1e47e89b2d43fc9ceee92
Kevin <kevin-batistac@hotmail.com>
2019-05-14 15:19:21 +0100

    JSON working

--- PubLib/src/main/java/pt/tecnico/sec/JSONGood.java
+++ PubLib/src/main/java/pt/tecnico/sec/JSONGood.java
@@ -13,76 +13,89 @@ import org.json.simple.parser.ParseException;
 
 public class JSONGood {
 	
-	public void updateFile(String goodID, String newOwner) {
-		 
+	private final String PATH = "goods";
+	
+
+	//manipulatorID is the ID of the entity that is using this method (for example notary1)
+	public void updateFile(String goodID, String newOwner,String goodState, String manipulatorID) {
+		File myFile=null;
 		try {
-			File myFile = new File("goods.json");
+			myFile = new File(PATH+manipulatorID+".json");
 			myFile.createNewFile();
 			    
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		JSONArray newUserList = new JSONArray();
 		
-		JSONParser jsonParser = new JSONParser();
-		try ( FileReader reader = new FileReader("goods.json")){
-			Object obj = jsonParser.parse(reader);
+		if(myFile.length()>0) {
+			JSONParser jsonParser = new JSONParser();
+			try ( FileReader reader = new FileReader(PATH+manipulatorID+".json")){
 			
-			JSONArray userList = (JSONArray) obj;
-			JSONArray newUserList = new JSONArray();
 			
-			for( Object userObject : userList) {
-				JSONObject user = (JSONObject)userObject;
-				JSONObject good = new JSONObject();
-				good.put(goodID, "n");
+				Object obj = jsonParser.parse(reader);
 				
-				System.out.println("user: "+user);
-				System.out.println("value: "+user.get("userID"));
-				System.out.println("value: "+user.get("goods"));
+				JSONArray userList = (JSONArray) obj;
 				
-				if(user.get(goodID)!=null) {
-					user.remove(goodID);
-					System.out.println("object removed: "+user);
+				for( Object userObject : userList) {
+					JSONObject user = (JSONObject)userObject;
+					JSONObject goods = (JSONObject)user.get("goods");
 					
-				}else {
-					System.out.println("owner found");
-					user.put(newOwner,good);
+					if(!user.get("userID").equals(newOwner) && goods.get(goodID)!=null) {
+						goods.remove(goodID);
+						user.put("goods",goods);
+					}else if(user.get("userID").equals(newOwner)){
+						goods.put(goodID,goodState);
+						user.put("goods",goods);
+					}
+					newUserList.add(user);
+					
 				}
-				newUserList.add(user);
+				
+			try( FileWriter writer = new FileWriter(PATH+manipulatorID+".json")){
+				writer.write(newUserList.toJSONString());
+				writer.flush();
 			}
-//			JSONObject good = new JSONObject();
-//			good.put(goodID, "n");
-//			JSONObject user = new JSONObject();
-//			user.put(newOwner, good);
-//			newUserList.add(user);
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}else {
+			JSONObject user = new JSONObject();
+			JSONObject goods = new JSONObject();
+			goods.put(goodID,goodState);
+			user.put("goods",goods);
+			user.put("userID",newOwner);
+			newUserList.add(user);
 			
-//		try( FileWriter writer = new FileWriter("goods.json")){
-//			writer.write(newUserList.toJSONString());
-//			writer.flush();
-//		}
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			try( FileWriter writer = new FileWriter(PATH+manipulatorID+".json")){
+				writer.write(newUserList.toJSONString());
+				writer.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-		
 	}
 	
-	public boolean getGood( String goodID,String userID) {
+	//manipulatorID is the ID of the entity that is using this method (for example notary1)
+	public String findUser( String goodID,String manipulatorID) {
 		try {
-			File myFile = new File("goods.json");
+			File myFile = new File(PATH+manipulatorID+".json");
 			if(myFile.createNewFile())
-				return false;
+				return null;
 			
 			JSONParser jsonParser = new JSONParser();
-			try ( FileReader reader = new FileReader("goods.json")){
+			try ( FileReader reader = new FileReader(PATH+manipulatorID+".json")){
 				Object obj = jsonParser.parse(reader);
 				
 				JSONArray userList = (JSONArray) obj;
@@ -91,8 +104,8 @@ public class JSONGood {
 				for( Object userObject : userList) {
 					JSONObject user = (JSONObject)userObject;
 					JSONObject goods = (JSONObject)user.get("goods");
-					if(user.get("userID").equals(userID) && goods.get(goodID)!=null)
-						return true;
+					if(goods.get(goodID)!=null)
+						return user.get("userID").toString();
 				}
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
@@ -103,7 +116,7 @@ public class JSONGood {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		System.out.println("Not found");
-		return false;
+		return null;
 	}
+	
 }
