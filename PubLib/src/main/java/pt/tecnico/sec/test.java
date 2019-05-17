52241be75f84e4075d5d3c4e2e85d27f319f8058
Kevin <kevin-batistac@hotmail.com>
2019-05-17 09:23:12 +0100

    Update test.java

--- PubLib/src/main/java/pt/tecnico/sec/test.java
+++ PubLib/src/main/java/pt/tecnico/sec/test.java
@@ -27,27 +27,30 @@ public class test {
 //		else
 //			System.out.println("MATEMATICO");
 		
+	try {
+		digest = MessageDigest.getInstance("SHA-1");
+	} catch (NoSuchAlgorithmException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	Pair powHash= powHash("Hello World!");
-	System.out.println(verifyHash(powHash.getValue()+"",powHash.getKey()+""));
+	System.out.println(verifyHash(powHash.getKey()+"",powHash.getValue()+""));
 		
 	}
+	static MessageDigest digest;
 	static String hashLimit ="0000";
-	public static boolean verifyHash(String content,String hashValue) {
-		content = content+hashValue;
-		System.out.println(content);
-		MessageDigest digest;
+	public static boolean verifyHash(String hashValue,String content) {
+		String testContent = content+hashValue;
+		System.out.println(testContent);
 		byte[] hash = null;
 		for(int i = 0 ; i<5;i++) {
-		try {
-			digest = MessageDigest.getInstance("SHA-256");
+			content = testContent;
+			System.out.println(content);
 			hash = digest.digest(content.getBytes(StandardCharsets.UTF_8));
 			System.out.println(hash);
 			content = DatatypeConverter.printHexBinary(hash);
 			content = content.substring(0,4);
 			System.out.println(content);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
 		}
 		if(content.equals(hashLimit))
 			return true;
@@ -57,24 +60,22 @@ public class test {
 	
 	public static Pair powHash(String content) {
 		/* Returns string i such that content+i hashes to a string with hashLimit in the beginning */
-		MessageDigest digest;
+		String originalContent = content;
 		byte[] hash = null;
 		String hashString= null;
 		int i=0;
 		do {
-			hashString = content+i;
-			try {
-				digest = MessageDigest.getInstance("SHA-256");
+			hashString = originalContent+i;
+			System.out.println(hashString);
 				hash = digest.digest(hashString.getBytes(StandardCharsets.UTF_8));
 				content = DatatypeConverter.printHexBinary(hash);
-				hashString = hashString.substring(0,4);
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			}
+				hashString = content.substring(0,4);
+				System.out.println("hashString: "+hashString);
 			i++;
 		}
 		while(!hashString.equals(hashLimit)); 
-		return new Pair(i,content);
+		System.out.println(i);
+		return new Pair(i,originalContent);
 	}
 
 }
